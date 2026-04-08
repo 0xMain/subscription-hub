9695d26128508ed3500e6d489ecb61dfe427501c
0xMain <0xMain@users.noreply.github.com>
2026-04-08 17:06:00 +0300

    [shub] fix: увеличить время ожидания Vault в скрипте

--- scripts/vault/init.sh
+++ scripts/vault/init.sh
@@ -3,7 +3,7 @@ set -e
 
 export VAULT_ADDR="${VAULT_ADDR:-http://vault:8200}"
 export VAULT_TOKEN="${VAULT_TOKEN:-admin-token}"
-MAX_WAIT=0
+MAX_WAIT=10
 
 i=0
 until vault status > /dev/null 2>&1 || [ $? -eq 2 ] || [ $i -ge $MAX_WAIT ]; do
